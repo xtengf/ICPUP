4332ded68fbfa979659b2db3a6e84b512a4ed98c
xtengf <xtengf@gmail.com>
2015-10-15 01:51:05 +0800

    Chapter1--3

---
+++ cube_root_for.py
@@ -0,0 +1,10 @@
+x = int(raw_input('Enter an integer: '))
+for ans in range(0, abs(x)+1):
+    if ans**3 >= abs(x):
+        break
+if ans**3 != abs(x):
+    print x, 'is not a perfect cube'
+else:
+    if x < 0:
+        ans = -ans
+    print 'Cube root of', x, 'is', ans
---
+++ newton_raphson.py
@@ -0,0 +1,6 @@
+epsilon = 0.01
+k = 24.0
+guess = k/2.0
+while abs(guess*guess - k) >= epsilon:
+    guess = guess - (((guess**2) - k)/(2*guess))
+print 'Square root of', k, 'is about', guess
